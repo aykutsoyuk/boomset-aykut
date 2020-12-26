ddcfb568d774b882e02e3a4691d294c04c5b7a88
“aykutsoyuk” <aykutsoyuk@gmail.com>
2020-12-26 22:54:48 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    var requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: "Token ed56d8f5cbc4f810ff1a7a5ccb7af8e6",
+      },
+      redirect: "follow",
+    };
+    fetch("/events", requestOptions)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  },
+};
 </script>
 
 <style>
